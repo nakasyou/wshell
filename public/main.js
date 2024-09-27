ffb4d2afc2fc8b64741c7e612c9ca7efdf47ba2e
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2024-09-27 20:45:46 +0900

    Create main.js

---
+++ public/main.js
@@ -0,0 +1,22 @@
+const uint8ArrayToB64 = (data: Uint8Array) => btoa([...data].map(n => String.fromCharCode(n)).join(''))
+const b64ToUint8Array = (data: string) => new Uint8Array([...atob(data)].map(s => s.charCodeAt(0)))
+
+const $stdin = document.getElementById('stdin')
+const $shell = document.getElementById('shell')
+const $send = document.getElementById('send')
+
+const shellId = (await fetch('/api/create-shell').then(res => res.json())).id
+
+$send.onclick = async () => {
+  await fetch(`/api/stdin/${shellId}`, {
+    method: 'POST',
+    body: $stdin.value
+  })
+}
+
+const update = async () => {
+  console.log(await fetch(`/api/stdout/${shellId}`).then(res => res.json()))
+  
+  setTimeout(update, 100)
+}
+update()
